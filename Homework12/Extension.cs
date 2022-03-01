87e6edabf16766ef7dc83b96f01b53d52a7464db
Ismayil9918 <ismayilbi@code.edu.az>
2022-03-01 08:03:49 +0400

    Home work

---
+++ Homework12/Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework12
+{
+    static class Extension
+    {
+        public static string isCapitalized(this string word)
+        {
+            word = word.ToLower();
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(char.ToUpper(word[0]).ToString());
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                stringBuilder.Append(word[i]);
+            }
+
+            return stringBuilder.ToString();
+
+        }
+    }
+}
---
+++ Homework12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "exTensiON claSSi staTIc OLMalIdiR";
+            Console.WriteLine(name.isCapitalized());
+        }
+    }
+}
